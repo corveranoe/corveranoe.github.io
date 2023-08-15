7b5c6751f856c032b8dc747f5f7c2df2d89556b3
Noe Francisco Corvera - systems engineering student <corveranoe1995@outlook.es>
2023-08-15 17:07:52 -0600

    Cambios en el js
    Cambios aplicados

--- js/botonAprender.js
+++ js/botonAprender.js
@@ -1,7 +1,17 @@
-document.getElementById("cta").addEventListener("click", function() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const ctaLink = document.getElementById("ctaLink");
   
+  ctaLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    const targetElement = document.querySelector(ctaLink.getAttribute("href"));
+    
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth"
+      });
+    }
+  });
+});
